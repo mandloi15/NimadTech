b204177933befc7ccda967a0811be981a072603e
mandloi15 <mandloikratik600@gmail.com>
2026-05-16 15:48:09 +0530

    add method to generate access token in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -73,6 +73,20 @@ userSchema.method.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            role: this.role
+        },
 
+        process.env.ACCESS_TOKEN_SECRET,
+
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
 
 export default userSchema
